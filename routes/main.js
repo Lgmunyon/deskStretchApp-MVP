8731451537721308fb90acf54161edb2619f2610
Levi Munyon <lgmunyon@hotmail.com>
2022-11-11 17:19:02 -0500

    fetch url

--- routes/main.js
+++ routes/main.js
@@ -26,3 +26,20 @@ router.post("/signup", authController.postSignup);
 router.get("/stretches", authController.getStretches);
 
 module.exports = router;
+
+//Fetch API
+document.querySelector('button').addEventListener('click', getFetch) //Swap 'button' for 'a'? Give each body part individual fetch request?
+
+function getFetch() {
+    const choice = document.querySelector('input').value
+    const url = `https://fetch-a-stretch-api.vercel.app/api/${choice}`
+
+    fetch(url)
+    .then(res => res.json()) //parse response as JSON
+    .then(data => {
+        console.log(data)
+    })
+    .catch(err => {
+        console.log(`error ${err}`)
+    })
+}
